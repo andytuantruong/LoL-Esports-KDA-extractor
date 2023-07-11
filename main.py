d181a071e76b308f4765701674ccacd23cf877ed
andytuantruong <andytuantruong@gmail.com>
2023-07-11 15:32:24 -0700

    add win/loss variables
    added more statistical information to differentiate between wins and losses.

--- main.py
+++ main.py
@@ -10,10 +10,19 @@ def index():
 def process():
     data = request.form.get('data')
     lines = data.strip().split('\n')
+    
+    #initial variables
     extracted_data = []
-
     total_kills = 0
     num_matches = len(lines)
+
+    num_victories = 0
+    total_victory_kills = 0
+    average_victory_kills = 0
+
+    num_losses = 0
+    total_loss_kills = 0
+    average_loss_kills = 0
 
     for line in lines:
         line = line.strip()
@@ -34,10 +43,21 @@ def process():
         total_kills += kills
         extracted_data.append((champion, outcome, kda, kills, deaths, assists, game_time, match_date, match_details))
 
+        #tracks amount of victories in the dataset 
+        if outcome == "Victory":
+            num_victories += 1
+            total_victory_kills += kills
+        else:
+            num_losses += 1
+            total_loss_kills += kills
+
     average_kills = round(total_kills / num_matches, 3)
+    average_victory_kills = round(total_victory_kills / num_victories, 3)
+    average_loss_kills = round(total_loss_kills / num_losses, 3)
     kills_in_two_maps = average_kills * 2
 
-    return render_template('index.html', extracted_data=extracted_data, total_kills=total_kills, num_matches=num_matches, average_kills=average_kills, kills_in_two_maps=kills_in_two_maps)
-
+    return render_template('index.html', extracted_data=extracted_data, total_kills=total_kills, num_matches=num_matches, 
+                           average_kills=average_kills, kills_in_two_maps=kills_in_two_maps, num_victories=num_victories,
+                           num_losses=num_losses, average_victory_kills=average_victory_kills, average_loss_kills=average_loss_kills)
 if __name__ == '__main__':
     app.run()
